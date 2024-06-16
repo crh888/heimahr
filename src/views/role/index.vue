b507fcadd7969423d97de10719c4db9d08b60558
crh888 <nice235@163.com>
2024-06-16 23:11:43 +0800

    完成新增角色功能

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -6,6 +6,7 @@
         <el-button
           type="primary"
           size="mini"
+          @click="showDialog = true"
         >
           添加角色
         </el-button>
@@ -76,20 +77,104 @@
         />
       </el-row>
     </div>
+    <el-dialog
+      title="新增角色"
+      width="500px"
+      :visible.sync="showDialog"
+      @close="btnCancel"
+    >
+      <el-form
+        ref="roleForm"
+        :model="roleForm"
+        :rules="rules"
+        label-width="120px"
+      >
+        <el-form-item
+          prop="name"
+          label="角色名称"
+        >
+          <el-input
+            v-model="roleForm.name"
+            style="width: 300px"
+            size="mini"
+          />
+        </el-form-item>
+        <el-form-item
+          prop="state"
+          label="启用"
+        >
+          <!-- 重置表单 需要写 prop -->
+          <el-switch
+            v-model="roleForm.state"
+            :active-value="1"
+            :inactive-value="0"
+            size="mini"
+          />
+        </el-form-item>
+        <el-form-item
+          prop="description"
+          label="角色描述"
+        >
+          <el-input
+            v-model="roleForm.description"
+            type="textarea"
+            :rows="3"
+            style="width: 300px"
+            size="mini"
+          />
+        </el-form-item>
+        <el-form-item>
+          <el-row
+            type="flex"
+            justify="center"
+          >
+            <el-col :span="12">
+              <el-button
+                size="mini"
+                type="primary"
+                @click="btnOK"
+              >
+                确定
+              </el-button>
+              <el-button
+                size="mini"
+                @click="btnCancel"
+              >
+                取消
+              </el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getRoleList } from '@/api/role'
+import { getRoleList, addRole } from '@/api/role'
 export default {
   name: 'Role',
   data() {
     return {
       list: [],
+      showDialog: false, // 控制弹层显示隐藏
       // 分页信息
       pageParams: {
         page: 1, // 第几页
         pagesize: 5, // 每页多少条,
         total: 0
+      },
+      roleForm: {
+        name: '',
+        description: '',
+        state: 0 // 1 是启用 0 是未启用
+      },
+      rules: {
+        name: [
+          { required: true, message: '角色名称不能为空', trigger: 'blur' }
+        ],
+        description: [
+          { required: true, message: '角色名称不能为空', trigger: 'blur' }
+        ]
       }
     }
   },
@@ -105,6 +190,20 @@ export default {
     changePage(newPage) {
       this.pageParams.page = newPage
       this.getRoleList()
+    },
+    btnOK() {
+      this.$refs.roleForm.validate(async isOK => {
+        if (isOK) {
+          await addRole(this.roleForm)
+          this.$message.success('新增角色成功')
+          this.getRoleList()
+          this.btnCancel()
+        }
+      })
+    },
+    btnCancel() {
+      this.$refs.roleForm.resetFields()
+      this.showDialog = false
     }
   }
 }
