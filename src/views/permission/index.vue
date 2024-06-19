3864dacff41ecf77e3d50dcbc1f0f2bd54ef92ef
crh888 <nice235@163.com>
2024-06-19 23:28:42 +0800

    完成了权限页面的搭建

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="container">
     <div class="app-container">
-      权限管理
+      <el-button
+        class="btn-add"
+        size="mini"
+        type="primary"
+      >
+        添加权限
+      </el-button>
+
+      <el-table>
+        <el-table-column label="名称" />
+        <el-table-column label="标识" />
+        <el-table-column label="描述" />
+        <el-table-column label="操作">
+          <el-button
+            type="text"
+            size="mini"
+          >
+            添加
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+          >
+            编辑
+          </el-button>
+          <el-button
+            type="text"
+            size="mini"
+          >
+            删除
+          </el-button>
+        </el-table-column>
+      </el-table>
     </div>
   </div>
 </template>
@@ -10,3 +42,10 @@ export default {
   name: 'Permission'
 }
 </script>
+
+<style>
+.btn-add {
+  margin: 10px;
+}
+</style>
+
